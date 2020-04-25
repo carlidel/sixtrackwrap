477c3ae86780016d0dd5af6beee78e07cf507406
carlidel95 <carlidel95@gmail.com>
2020-04-26 00:58:30 +0200

    add recursive accumulation and such

--- sixtrackwrap/__init__.py
+++ sixtrackwrap/__init__.py
@@ -25,13 +25,16 @@ def accumulate_and_return(r, alpha, th1, th2, n_sectors):
     count = np.zeros((r.shape[0], n_sectors, n_sectors), dtype=np.int32)
 
     for j in prange(r.shape[0]):
-        matrix = np.zeros((n_sectors, n_sectors)) 
+        matrix = np.zeros((n_sectors, n_sectors)) * np.nan
         
         for k in range(r.shape[1]):
-            matrix[i_1[j, k], i_2[j, k]] = (
-                (matrix[i_1[j, k], i_2[j, k]] * count[j, i_1[j, k],
-                                                      i_2[j, k]] + r[j, k]) / (count[j, i_1[j, k], i_2[j, k]] + 1)
-            )
+            if count[j, i_1[j, k], i_2[j, k]] == 0:
+                matrix[i_1[j, k], i_2[j, k]] = r[j, k]
+            else:  
+                matrix[i_1[j, k], i_2[j, k]] = (
+                    (matrix[i_1[j, k], i_2[j, k]] * count[j, i_1[j, k],
+                                                        i_2[j, k]] + r[j, k]) / (count[j, i_1[j, k], i_2[j, k]] + 1)
+                )
             count[j, i_1[j, k], i_2[j, k]] += 1
         
         for a in range(matrix.shape[0]):
@@ -43,6 +46,31 @@ def accumulate_and_return(r, alpha, th1, th2, n_sectors):
         matrices[j, :, :] = matrix
 
     return count, matrices, result
+
+
+def recursive_accumulation(count, matrices):
+    n_sectors = count.shape[1]
+    c = []
+    m = []
+    r = []
+    validity = []
+    c.append(count.copy())
+    m.append(matrices.copy())
+    r.append(np.nanmean(np.power(matrices, 4), axis=(1, 2)))
+    while n_sectors >= 2 and n_sectors % 2 == 0:
+        matrices *= count
+        count = np.nansum(count.reshape(
+            (count.shape[0], n_sectors//2, 2, n_sectors//2, 2)), axis=(2, 4))
+        matrices = np.nansum(matrices.reshape(
+            (matrices.shape[0], n_sectors//2, 2, n_sectors//2, 2)), axis=(2, 4)) / count
+        result = np.nanmean(np.power(matrices, 4), axis=(1, 2))
+        c.append(count.copy())
+        m.append(matrices.copy())
+        r.append(result.copy())
+        validity.append(np.logical_not(
+            np.any(np.isnan(matrices), axis=(1, 2))))
+        n_sectors = n_sectors // 2
+    return c, m, r, np.asarray(validity, dtype=np.bool)
 
 
 def track_particles(x, px, y, py, n_turns):
